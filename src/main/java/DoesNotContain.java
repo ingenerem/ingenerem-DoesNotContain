63e838b14c2836da7109620b146ab9087a75e966
ingenerem Marie <ingenerem@gmail.com>
2025-01-24 23:15:23 +0000

    Does Not Contain

--- src/main/java/DoesNotContain.java
+++ src/main/java/DoesNotContain.java
@@ -15,6 +15,12 @@ public class DoesNotContain {
      * @return true if arr does not contain target. false otherwise.
      */
     public boolean arrayDoesNotContain(int[] arr, int target){
-        return false;
+        if(arr.length !=0){
+        for(int i =0; i < arr.length; i ++){
+            if(arr[i]== target)
+              return false;
+        }
+        }
+        return true;
     }
 }
